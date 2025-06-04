3b2dc11f4af7f89f387ad3f71a1d5f61652b8d92
haracena <haracena.dev@gmail.com>
2025-06-04 14:12:26 -0400

    Adds discord subs get endpoint

---
+++ app/api/discord/route.ts
@@ -0,0 +1,93 @@
+import { NextRequest, NextResponse } from "next/server";
+import { avalancheFuji } from "viem/chains";
+import {
+  createMetadata,
+  Metadata,
+  ValidatedMetadata,
+  ExecutionResponse,
+} from "@sherrylinks/sdk";
+import { serialize } from "wagmi";
+import { encodeFunctionData, TransactionSerializable } from "viem";
+// import { abi } from "./blockchain/abi";
+
+export async function GET(req: NextRequest) {
+  try {
+    const host = req.headers.get("host") || "localhost:3000";
+    const protocol = req.headers.get("x-forwarded-proto") || "http";
+    const serverUrl = `${protocol}://${host}`;
+
+    const metadata: Metadata = {
+      url: "https://sherry.social",
+      icon: "https://avatars.githubusercontent.com/u/117962315",
+      title: "Discord subscription",
+      baseUrl: serverUrl,
+      description: "Subscribe to the Discord server",
+      actions: [
+        {
+          type: "dynamic",
+          label: "Join the Discord server", // mensaje del botón
+          description: "Join the Discord server to get the latest updates",
+          chains: { source: "fuji" },
+          path: `/api/discord`,
+          params: [
+            {
+              name: "subscription_time",
+              label: "Subscription time",
+              type: "radio",
+              options: [
+                {
+                  label: "1 month",
+                  value: 1,
+                },
+                {
+                  label: "3 months",
+                  value: 3,
+                },
+                {
+                  label: "1 year",
+                  value: 12,
+                },
+              ],
+            },
+            {
+              name: "discord_id",
+              label: "Discord ID",
+              type: "text",
+              required: true,
+              description: "Enter your Discord ID not username",
+            },
+            {
+              name: "message",
+              label: "Message (optional)",
+              type: "text",
+              required: false,
+              description: "Send your greeting message",
+            },
+            {
+              name: "referral_address",
+              label: "Referral address (optional)",
+              type: "address",
+              required: false,
+              description: "Enter the referral address",
+            },
+          ],
+        },
+      ],
+    };
+
+    const validated: ValidatedMetadata = createMetadata(metadata);
+
+    return NextResponse.json(validated, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      },
+    });
+  } catch (error) {
+    console.error("Error creating metadata:", error);
+    return NextResponse.json(
+      { error: "Error creating metadata" },
+      { status: 500 }
+    );
+  }
+}
