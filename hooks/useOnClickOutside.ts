4c195b35ae3b467e75271a2a144626e0d4d306b6
Felipe <fernandez.2534@gmail.com>
2025-12-02 21:07:22 -0300

    Add click outside detection hook
    Implement useOnClickOutside hook for handling clicks outside specified elements

---
+++ hooks/useOnClickOutside.ts
@@ -0,0 +1,31 @@
+import { useEffect, RefObject } from 'react';
+
+/**
+ * Hook to detect clicks outside of specified element
+ * @param ref - Ref to element to monitor
+ * @param handler - Function to call when click outside is detected
+ */
+export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: MouseEvent | TouchEvent) => void
+): void {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      const element = ref?.current;
+
+      if (!element || element.contains(event.target as Node)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}
