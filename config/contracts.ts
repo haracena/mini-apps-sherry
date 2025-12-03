1499e32708154efe99168fdd5a2fdc4126f4ad91
Felipe <fernandez.2534@gmail.com>
2025-12-02 21:21:10 -0300

    Add DailyStreak contract address to config

--- config/contracts.ts
+++ config/contracts.ts
@@ -3,4 +3,5 @@
  */
 export const CONTRACTS = {
   TELEGRAM_GROUP_INVITATION: "0x9Da5D4De75832CD63666AC738837B88fCf4b3396" as `0x${string}`,
+  DAILY_STREAK: "0x0000000000000000000000000000000000000000" as `0x${string}`, // TODO: Update after deployment
 } as const;
