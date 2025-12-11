c4872bc318d94171617df1c5bb4b67c9c0041559
Felipe <fernandez.2534@gmail.com>
2025-12-11 15:37:35 -0300

    feat: add Stacks NFT gallery support with network selector

--- app/nft-gallery/page.tsx
+++ app/nft-gallery/page.tsx
@@ -3,8 +3,11 @@
 import { PageHeader } from "@/components/PageHeader";
 import { NFTGallery } from "@/components/nft/NFTGallery";
 import { PageTransition } from "@/components/PageTransition";
+import { NetworkSelector, useNetworkSelector } from "@/components/NetworkSelector";
 
 export default function NFTGalleryPage() {
+  const { network } = useNetworkSelector();
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 via-blue-900 to-black">
       <div className="container mx-auto px-4 py-8">
@@ -15,9 +18,16 @@ export default function NFTGalleryPage() {
           />
         </PageTransition>
 
+        {/* Network Selector */}
+        <PageTransition delay={50}>
+          <div className="flex justify-center mb-6">
+            <NetworkSelector />
+          </div>
+        </PageTransition>
+
         <PageTransition delay={100}>
           <div className="mt-8">
-            <NFTGallery />
+            <NFTGallery network={network} />
           </div>
         </PageTransition>
       </div>
